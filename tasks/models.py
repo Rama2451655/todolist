c1ec50023ff2fc5652e90a979fd0635a9e52e1e1
unknown <levakuramakrishnareddy4d2@gmail.com>
2025-04-19 17:13:06 +0530

    first commit

---
+++ tasks/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+class Task(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
---
+++ tasks/views.py
@@ -0,0 +1,33 @@
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Task
+from django.urls import reverse_lazy
+
+class TaskListView(LoginRequiredMixin, ListView):
+    model = Task
+    context_object_name = 'tasks'
+    template_name = 'tasks/task_list.html'
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
+
+class TaskCreateView(LoginRequiredMixin, CreateView):
+    model = Task
+    fields = ['title', 'description', 'completed']
+    template_name = 'tasks/task_form.html'
+    success_url = reverse_lazy('task-list')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class TaskUpdateView(LoginRequiredMixin, UpdateView):
+    model = Task
+    fields = ['title', 'description', 'completed']
+    template_name = 'tasks/task_form.html'
+    success_url = reverse_lazy('task-list')
+
+class TaskDeleteView(LoginRequiredMixin, DeleteView):
+    model = Task
+    template_name = 'tasks/task_confirm_delete.html'
+    success_url = reverse_lazy('task-list')
